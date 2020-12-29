1f617ff376fd90eb1efa5ac3d5a1a23cf8f1ea9e
yongjae5717 <76430761+yongjae5717@users.noreply.github.com>
2020-12-30 01:14:42 +0900

    Update login_page.dart

--- lib/Login/login_page.dart
+++ lib/Login/login_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/button_builder.dart';
 import './register_page.dart';
 import './signin_page.dart';
-import './loginpage.dart';
 
 class login_page extends StatelessWidget {
   // Navigates to a new page
@@ -18,7 +17,15 @@ class login_page extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("어라(UH-RA)"),
+        title: Text(
+          "어라(UH-RA)",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 3.0,
+            fontSize: 20,
+          ),
+        ),
         backgroundColor: Colors.orangeAccent,
       ),
       body: Column(
@@ -26,27 +33,49 @@ class login_page extends StatelessWidget {
         children: <Widget>[
           Padding(padding: EdgeInsets.only(top: 50)),
           Center(
-            child: Image(
-              image: AssetImage('pic/login.gif'),
-              width: 180.0,
-              height: 180.0,
+            child: CircleAvatar(
+              backgroundImage: AssetImage('pic/login.gif'),
+              radius: 100.0,
             ),
           ),
+          Padding(padding: const EdgeInsets.only(top: 50)),
           Container(
-            child: SignInButtonBuilder(
-              icon: Icons.person_add, fontSize: 20, height: 60, width: 300,
-              backgroundColor: Colors.indigo,
-              text: '회원가입', elevation: 0,
-              onPressed: () => _pushPage(context, RegisterPage()),
+            child: Text(
+              "안녕하세요. 어라(UH-RA)에 오신 것을 환영합니다\n만 14세 미만은 법정대리인 동의 후 회원서비스 이용이 가능합니다.\n선택약관에 동의하지 않아도 회원가입은 가능합니다.",
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 3.0,
+                fontSize: 15,
+              ),
             ),
-            padding: const EdgeInsets.only(top: 400),
             alignment: Alignment.center,
           ),
           Container(
             child: SignInButtonBuilder(
-              icon: Icons.verified_user, fontSize: 20, height: 60, width: 300,
+              icon: Icons.person_add,
+              fontSize: 20,
+              height: 60,
+              width: 300,
+              backgroundColor: Colors.indigo,
+              text: '회원가입',
+              elevation: 0,
+              splashColor: Colors.red,
+              onPressed: () => _pushPage(context, RegisterPage()),
+            ),
+            padding: const EdgeInsets.only(top: 300),
+            alignment: Alignment.center,
+          ),
+          Container(
+            child: SignInButtonBuilder(
+              icon: Icons.login,
+              fontSize: 20,
+              height: 60,
+              width: 300,
               backgroundColor: Colors.orange,
-              text: '로그인', elevation: 0,
+              text: '로그인',
+              elevation: 0,
+              splashColor: Colors.red,
               onPressed: () => _pushPage(context, SignInPage()),
             ),
             padding: const EdgeInsets.all(10),
