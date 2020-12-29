4aa5b2333d81c38ad78cdaaf8e34525825da54c4
yongjae5717 <76430761+yongjae5717@users.noreply.github.com>
2020-12-29 16:44:19 +0900

    Update home_page.dart

--- lib/Home/home_page.dart
+++ lib/Home/home_page.dart
@@ -1,116 +1,52 @@
-import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
+import 'package:flutter/material.dart';
+import 'package:exercise/Homepage.dart';
+
+class Nav extends StatefulWidget {
   @override
-  _HomePage createState() => _HomePage();
+  _State createState() => _State();
 }
 
-class _HomePage extends State<HomePage> {
-  bool _isStared = false;
-  int _count = 41;
+class _State extends State<Nav> {
+  int _selectedIndex = 0;
+  List<Widget> _WidgetOptions = <Widget>[
+    HomePage(),
+    Text('Calendar'),
+    Text('Play'),
+  ];
 
+  void _onItemTap(int index){
+    setState((){
+      _selectedIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    var titleSection = Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text("Oeschinen Lake Campground",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26)),
-            Text("Kandresteg Switzerland",
-                style: TextStyle(color: Colors.grey, fontSize: 26)),
-          ],
-        ),
-        Padding(padding: EdgeInsets.all(10.0)),
-        IconButton(
-          onPressed: _pressedStar,
-          icon: (_isStared ? Icon(Icons.star) : Icon(Icons.star_border)),
-          iconSize: 45,
-          color: Colors.red[500],
-        ),
-        Text(
-          "$_count",
-          style: TextStyle(fontSize: 30),
-        ),
-      ],
-    );
-
-    var buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            Icon(
-              Icons.call,
-              size: 45,
-              color: Colors.blue,
-            ),
-            Text(
-              "CALL",
-              style: TextStyle(color: Colors.blue),
-            ),
-          ],
-        ),
-        Padding(padding: EdgeInsets.all(20.0)),
-        Column(
-          children: <Widget>[
-            Icon(Icons.near_me, size: 45, color: Colors.blue),
-            Text(
-              "ROUTE",
-              style: TextStyle(color: Colors.blue),
-            ),
-          ],
-        ),
-        Padding(padding: EdgeInsets.all(20.0)),
-        Column(
-          children: <Widget>[
-            Icon(Icons.share, size: 45, color: Colors.blue),
-            Text(
-              "SHARE",
-              style: TextStyle(color: Colors.blue),
-            ),
-          ],
-        ),
-      ],
-    );
-
-    var textSection = Container(
-        child: Text(
-            'Spotler',
-            style: TextStyle(fontSize: 20)),
-        padding: EdgeInsets.all(30.0));
-
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        Image(
-          image: AssetImage('pic/pic1.png'),
-          height: 240,
-          width: 600,
-          fit: BoxFit.cover,
-        ),
-        //이미지 삽입
-        Padding(padding: EdgeInsets.all(15.0)),
-        titleSection,
-        Padding(padding: EdgeInsets.all(15.0)),
-        buttonSection,
-        //Padding(padding: EdgeInsets.all(15.0)),
-        textSection,
-      ],
-    ));
-  }
-
-  void _pressedStar() {
-    setState(() {
-      if (_isStared) {
-        _isStared = false;
-        _count -= 1;
-      } else {
-        _isStared = true;
-        _count += 1;
-      }
-    });
+      // appBar: AppBar(
+      //   title: Text('Spotler'),
+      // ),
+      body: Center(
+          child: _WidgetOptions.elementAt(_selectedIndex)   //page변환 넣으면 될거같음
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Play',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTap,
+      ),
+    );
   }
 }
