3b285a8319c1ec91c16fbe6195fcec9f2998fed0
yongjae5717 <76430761+yongjae5717@users.noreply.github.com>
2020-12-29 22:52:10 +0900

    Update login_page.dart

--- lib/Login/login_page.dart
+++ lib/Login/login_page.dart
@@ -16,12 +16,22 @@ class login_page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("어라(UH-RA)"),
+        backgroundColor: Colors.orangeAccent,
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
+          Padding(padding: EdgeInsets.only(top: 50)),
+          Center(
+            child: Image(
+              image: AssetImage('pic/login.gif'),
+              width: 180.0,
+              height: 180.0,
+            ),
+          ),
           Container(
             child: SignInButtonBuilder(
               icon: Icons.person_add, fontSize: 20, height: 60, width: 300,
@@ -29,7 +39,7 @@ class login_page extends StatelessWidget {
               text: '회원가입', elevation: 0,
               onPressed: () => _pushPage(context, RegisterPage()),
             ),
-            padding: const EdgeInsets.only(top: 700),
+            padding: const EdgeInsets.only(top: 400),
             alignment: Alignment.center,
           ),
           Container(
