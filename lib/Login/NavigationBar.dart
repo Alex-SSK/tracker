2db4812bffb94555405afed04e37daf532cbc089
yongjae5717 <76430761+yongjae5717@users.noreply.github.com>
2020-12-30 01:01:08 +0900

    Update NavigationBar.dart

--- lib/Login/NavigationBar.dart
+++ lib/Login/NavigationBar.dart
@@ -1,7 +1,5 @@
-
 import 'package:flutter/material.dart';
 import './Home_screen.dart';
-
 
 class Nav extends StatefulWidget {
   @override
@@ -11,40 +9,59 @@ class Nav extends StatefulWidget {
 class _State extends State<Nav> {
   int _selectedIndex = 0;
   List<Widget> _WidgetOptions = <Widget>[
+    Text('달려라'),
+    Text('들어라'),
     HomePage(),
-    Text('Page2'),
-    Text('Page3'),
+    Text('먹어라'),
+    Text('설정'),
   ];
 
-  void _onItemTap(int index){
-    setState((){
+  void _onItemTap(int index) {
+    setState(() {
       _selectedIndex = index;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   //title: Text('Spotler'),
-      // ),
       body: Center(
-        child: _WidgetOptions.elementAt(_selectedIndex)   //page변환 넣으면 될거같음
-      ),
+          child: _WidgetOptions.elementAt(_selectedIndex) //page변환 넣으면 될거같음
+          ),
+      backgroundColor: Colors.white,
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+            icon: Icon(Icons.directions_run,),
+            backgroundColor: Colors.white,
+            label: '달려라',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.note),
-            label: 'Note',
+            icon: Icon(Icons.arrow_upward_outlined),
+            backgroundColor: Colors.white,
+            label: '들어라',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Play',
-         ),
-       ],
+            icon: Icon(
+              Icons.home,
+            ),
+            backgroundColor: Colors.white,
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fastfood_outlined),
+            backgroundColor: Colors.white,
+            label: '먹어라',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            backgroundColor: Colors.white,
+            label: '설정',
+          ),
+        ],
+        selectedLabelStyle: TextStyle(fontSize: 20),
+        selectedItemColor: Colors.orangeAccent,
         currentIndex: _selectedIndex,
         onTap: _onItemTap,
       ),
